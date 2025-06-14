4aba5a7789dbc17d07554273cd532463557a793d
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-06-14 22:19:17 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://student-progress-management-system-api.onrender.com",
         changeOrigin: true,
       },
     },
